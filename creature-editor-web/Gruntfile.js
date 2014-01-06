6298366b166dbbfa0f9cafa0e2144d78c8757817
Arnaud <arnaudpourbaix@gmail.com>
2014-01-06 18:14:59 +0100

    correct bug in gruntfile related to karma config

--- creature-editor-web/Gruntfile.js
+++ creature-editor-web/Gruntfile.js
@@ -320,7 +320,7 @@ module.exports = function(grunt) {
 				configFile : '<%= build_dir %>/karma-unit.js'
 			},
 			unit : {
-				runnerPort : 9101,
+				port : 9877,
 				background : true
 			},
 			continuous : {
--- creature-editor-web/src/app/mod/mod.spec.js
+++ creature-editor-web/src/app/mod/mod.spec.js
@@ -1,8 +1,77 @@
 describe('mod section', function() {
 	"use strict";
-	beforeEach(module('creatureEditor.mod'));
+
+	var modListController, location, scope, state;
+	var stateProvider, locationProvider, templateParams, ctrlName;
+
+	beforeEach(function() {
+		// angular.mock.module('ui.router', []);
+		// angular.mock.module('ngRoute', []);
+		// angular.mock.module('ngResource', []);
+		// angular.mock.module('creatureEditor', []);
+		// angular.mock.module('creatureEditor.mod', [ 'ui.router', 'ngRoute', 'ngResource' ]);
+		angular.mock.module('creatureEditor.mod');
+	});
+
+	beforeEach(angular.mock.inject(function($rootScope, $controller){
+		scope = $rootScope.new();
+		modListController = $controller('ModListController', {
+			$scope: scope,
+		});
+	}));
 
 	it('should have a dummy test', angular.mock.inject(function() {
 		expect(true).toBeTruthy();
 	}));
+
+	// it('should have an empty events array', function(){
+	// console.log(scope);
+	// expect(scope.mods).toBe([]);
+	// });
+
+	// beforeEach(angular.mock.inject(function($state, $rootScope) {
+	// $rootScope.$apply(function() {
+	// $state.go("mods");
+	// state = $state;
+	// });
+	// }));
+
+	// beforeEach(angular.mock.inject(function($controller, $location, $rootScope) {
+	// location = $location;
+	// scope = $rootScope.$new();
+	// ModListController = $controller('ModListController', {
+	// $location : location,
+	// $scope : scope
+	// });
+	// }));
+
+	// beforeEach(angular.mock.inject(function($controller, $rootScope, $state, $stateParams) {
+	// scope = $rootScope.$new();
+	// ModListController = function() {
+	// $state.transitionTo('mods', {});
+	// $rootScope.$digest();
+	// return $controller('ModListController', {
+	// $scope : scope,
+	// $state : $state
+	// });
+	// };
+	// }));
+
+	// it('should have title for about set', angular.mock.inject(function() {
+	// var controller = createController();
+	// expect(scope.mods).toBeTruthy();
+	// }));
+
+	// it('state should be mods', angular.mock.inject(function() {
+	// expect(state.current.name).to.equal("mods");
+	// expect(true).toBeTruthy();
+	// expect(ModListController).toBeTruthy();
+	// }));
+
+	// it('should create "phones" model with 3 phones', function() {
+	// expect(true).toBeTruthy();
+	// var scope = {},
+	// ctrl = new PhoneListCtrl(scope);
+	// expect(scope.phones.length).toBe(3);
+	// });
 });
