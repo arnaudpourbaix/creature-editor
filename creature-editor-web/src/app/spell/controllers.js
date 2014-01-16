772bccbd1db9c137579e853c259b484aee12c057
Arnaud <arnaudpourbaix@gmail.com>
2014-01-16 08:41:37 +0100

    add log4j for test add more code to test deferred result

--- creature-editor-web/src/app/spell/controllers.js
+++ creature-editor-web/src/app/spell/controllers.js
@@ -1,3 +1,91 @@
+var allow = true, request;
+var startUrl;
+var pollUrl;
+
+function Poll() {
+	'use strict';
+
+	this.start = function(start, poll) {
+
+		startUrl = start;
+		pollUrl = poll;
+
+		if (request) {
+			request.abort(); // abort any pending request
+		}
+
+		// fire off the request to MatchUpdateController
+		request = $.ajax({
+			url : startUrl,
+			type : "get",
+		});
+
+		// This is jQuery 1.8+
+		// callback handler that will be called on success
+		request.done(function(reply) {
+
+			console.log("Game on..." + reply);
+			setInterval(function() {
+				if (allow === true) {
+					allow = false;
+					getUpdate();
+				}
+			}, 500);
+		});
+
+		// callback handler that will be called on failure
+		request.fail(function(jqXHR, textStatus, errorThrown) {
+			// log the error to the console
+			console.log("Start - the following error occured: " + textStatus, errorThrown);
+		});
+
+	};
+
+	function getUpdate() {
+
+		console.log("Okay let's go...");
+
+		if (request) {
+			request.abort(); // abort any pending request
+		}
+
+		// fire off the request to MatchUpdateController
+		request = $.ajax({
+			url : pollUrl,
+			type : "get",
+		});
+
+		// This is jQuery 1.8+
+		// callback handler that will be called on success
+		request.done(function(message) {
+			console.log("Received a message");
+
+			var update = getMessage(message);
+			$(update).insertAfter('.spell-import-progress');
+		});
+
+		function getMessage(message) {
+
+			var update = "<div class='span-4  prepend-2'>" + "<p class='update'>Time:</p>" + "</div>" + "<div class='span-3 border'>" + "<p id='time' class='update'>"
+					+ message.matchTime + "</p>" + "</div>" + "<div class='span-13 append-2 last' id='update-div'>" + "<p id='message' class='update'>" + message.messageText + "</p>"
+					+ "</div>";
+			return update;
+		}
+
+		// callback handler that will be called on failure
+		request.fail(function(jqXHR, textStatus, errorThrown) {
+			// log the error to the console
+			console.log("Polling - the following error occured: " + textStatus, errorThrown);
+			allow = false;
+		});
+
+		// callback handler that will be called regardless if the request failed or succeeded
+		request.always(function() {
+			allow = true;
+		});
+	}
+}
+
 (function() {
 	'use strict';
 
@@ -58,20 +146,26 @@
 
 		$scope.import = function() {
 			$scope.progressValue = 0;
-			$http({
-				method : 'GET',
-				url : 'rest/spell/import',
-				params : {
-					modId : $scope.mod.id
-				},
-			}).then(function(data) {
-				console.log("Game on...", data);
-				stop = $interval(function() {
-					$scope.getUpdate();
-				}, 2000);
-			}, function(data, status, headers, config) {
-				console.log('error', data);
-			});
+			var startUrl = "rest/spell/import?modId=1";
+			var pollUrl = "rest/spell/deferred";
+
+			var poll = new Poll();
+			poll.start(startUrl, pollUrl);
+
+			// $http({
+			// method : 'GET',
+			// url : 'rest/spell/import',
+			// params : {
+			// modId : $scope.mod.id
+			// },
+			// }).then(function(data) {
+			// console.log("Game on...", data);
+			// stop = $interval(function() {
+			// $scope.getUpdate();
+			// }, 2000);
+			// }, function(data, status, headers, config) {
+			// console.log('error', data);
+			// });
 		};
 
 		$scope.getUpdate = function() {
