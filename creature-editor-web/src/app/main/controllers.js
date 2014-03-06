d91312098481ad66a7164aab8fec2de5fe07568a
Arnaud <arnaudpourbaix@gmail.com>
2014-03-06 22:13:34 +0100

    correct alert message new css

--- creature-editor-web/src/app/main/controllers.js
+++ creature-editor-web/src/app/main/controllers.js
@@ -3,22 +3,16 @@
 
 	var module = angular.module('creatureEditor.main.controllers', [ 'pascalprecht.translate' ]);
 
-	module.controller('MainController', [ '$scope', '$translate', '$rootScope', '$interval', '$alertMessage', function MainController($scope, $translate, $rootScope, $interval, $alertMessage) {
-		
+	module.controller('MainController', [ '$scope', '$translate', '$rootScope', function MainController($scope, $translate, $rootScope) {
+
 		$scope.$onRootScope('$translateChangeSuccess', function() {
 			$scope.langKey = $translate.use();
 		});
-		
+
 		$scope.changeLanguage = function(langKey) {
 			$translate.use(langKey);
 		};
-		
-		$interval(function() {
-			$alertMessage.push('SPELL.IMPORT.WARNING_MESSAGE', 'info', {
-				name : 'Dummy Dummy Dummy'
-			});
-		}, 2000);
-		
+
 	} ]);
 
 })();
