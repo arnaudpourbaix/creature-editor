e7cfc3db2e8ec842b4266ca04237ab0300f215a6
Arnaud <arnaudpourbaix@gmail.com>
2014-03-25 22:24:37 +0100

    working jq-docking good enough to move on spell layout WIP

--- creature-editor-web/src/app/spell/config.js
+++ creature-editor-web/src/app/spell/config.js
@@ -14,6 +14,7 @@
 			url : '/spells',
 			controller : 'SpellController',
 			templateUrl : 'spell/spell.tpl.html',
+			// template: '<div data-jq-docking="layout"></div>',
 			resolve : {
 				mods : [ 'Mod', function(Mod) {
 					return Mod.query().$promise;
--- creature-editor-web/src/common/jqwidgets/jq-docking.js
+++ creature-editor-web/src/common/jqwidgets/jq-docking.js
@@ -9,57 +9,36 @@
 
 		this.$get = [ '$jqCommon', function jqDockingService($jqCommon) {
 			var service = {};
-				
+
 			service.options = function() {
-					return options;
+				return options;
 			};
-			
-			service.setWindowProperties = function(element, id, properties) {
+
+			service.setWindowProperties = function(element, properties) {
 				var props = {};
-				if (properties.height) {
+				if (properties.height && properties.height > 600) {
 					props.maxHeight = properties.height;
 				}
-				if (properties.width) {
-					props.maxWidth = properties.width;
-				}
-				angular.extend(props, _.omit(properties, [ 'template', 'templateUrl' ]));
-				console.log(props);
+				angular.extend(props, _.omit(properties, [ 'id', 'template', 'templateUrl' ]));
 				angular.forEach(props, function(value, key) {
-					console.log(id, key, value);
-					element.jqxDocking('setWindowProperty', id, key, value);
+					element.jqxDocking('setWindowProperty', properties.id, key, value);
 				});
 			};
-			
+
 			return service;
 		} ];
 	});
 
-	module.directive('jqDocking', [ '$compile', '$timeout', '$q', '$jqCommon', '$jqDocking', function JqDockingDirective($compile, $timeout, $q, $jqCommon, $jqDocking) {
+	module.directive('jqDocking', [ '$compile', '$timeout', '$jqCommon', '$jqDocking', function JqDockingDirective($compile, $timeout, $jqCommon, $jqDocking) {
 		return {
 			restrict : 'AE',
-			template : '<div class="jq-docking"><div data-ng-scope-element="jqDockingWindows"></div></div>',
-			replace : true,
-			compile : function() {
-				return {
-					post : function($scope, iElement, iAttrs) {
-						var getParams = function() {
-							return $jqCommon.getParams($scope.$eval(iAttrs.jqDocking), [ 'windows' ], [ 'options' ]);
-						};
-						var params = getParams();
-						var promises = [];
-						angular.forEach(params.windows, function(win) {
-							promises.push($jqCommon.getView(win, null, null, $scope));
-						});
-						$q.all(promises).then(function(views) {
-							$scope.jqDockingWindows.append(views);
-							var options = angular.extend({}, $jqCommon.options(), $jqDocking.options(), params.options);
-							iElement.jqxDocking(options);
-							angular.forEach(params.windows, function(win, index) {
-								$jqDocking.setWindowProperties(iElement, views[index].attr('id'), win);
-							});
-						});
-					}
-				};
+			link : function($scope, iElement, iAttrs) {
+				var params = $jqCommon.getParams($scope.$eval(iAttrs.jqDocking), [ 'windows' ], [ 'options' ]);
+				var options = angular.extend({}, $jqCommon.options(), $jqDocking.options(), params.options);
+				iElement.jqxDocking(options);
+				angular.forEach(params.windows, function(w) {
+					$jqDocking.setWindowProperties(iElement, w);
+				});
 			}
 		};
 	} ]);
